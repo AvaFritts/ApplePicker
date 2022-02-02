e8fccd1f3a0fc7b2539c4db7c3bb8f8e290be44a
AvaFritts <avanfritts@gmail.com>
2022-02-02 15:21:54 -0600

    Updated scripts
    Completed pages 476 - 484

--- ApplePicker-Unity/Assets/Scripts/Basket.cs
+++ ApplePicker-Unity/Assets/Scripts/Basket.cs
@@ -13,6 +13,26 @@ public class Basket : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //current screen position
+        Vector3 mousPos2D = Input.mousePosition;
+        //Puts mouse in 3d space
+        mousPos2D.z = -Camera.main.transform.position.z;
+        //converts 2d to 3d
+        Vector3 mousPos3D = Camera.main.ScreenToWorldPoint(mousPos2D);
+
+        //attaches basket to mouse
+        Vector3 pos = this.transform.position;
+        pos.x = mousPos3D.x;
+        this.transform.position = pos;
+    }
+
+    private void OnCollisionEnter(Collision coll)
+    {
+        //determin the hit
+        GameObject collidedWith = coll.gameObject;
+        if (collidedWith.tag == "Apple")
+        {
+            Destroy(collidedWith);
+        }
     }
 }
